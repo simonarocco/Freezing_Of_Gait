80f879a7a1c2e01177a4f3399c62463469bb08f7
mayaliliya <maya.pavlova4816@gmail.com>
2018-11-28 21:33:33 -0500

    Added real time signal function

--- Python Code/Data_to_Signal.py
+++ Python Code/Data_to_Signal.py
@@ -1,11 +1,31 @@
 import pandas as pd
 from enum import Enum
 import numpy as np
+import serial
+from Serial_Monitor_Reading import process_string
 
-#enum class for accelerations
+# enum class for accelerations
 class Acceleration(Enum):
     X = "X Acceleration"
     Y = "Y Acceleration"
+
+
+def get_raw_signal(accel_type):
+    ser = serial.Serial("/dev/cu.usbmodem1451", 9600)
+    ser.reset_input_buffer()
+    acz = []
+    acy = []
+    acx = []
+    count = 0
+    while count <= 240:
+        raw_string = str(ser.readline())
+        if "Acc z" in raw_string:
+            acz.append(process_string(raw_string))
+        elif "Acc y" in raw_string:
+            acy.append(process_string(raw_string))
+        elif "Acc x" in raw_string:
+            acx.append(process_string(raw_string))
+    return acx[:80] if accel_type == Acceleration.X else acy[:80]
 
 def get_signal(file_path, accel_type):
     df = pd.read_csv(file_path)
@@ -16,6 +36,7 @@ def get_signal(file_path, accel_type):
     elif accel_type == Acceleration.Y:
         return y_accel
 
+
 def smooth_signal(signal, window_size):
     window = np.array([np.nan] * window_size)
     smoothed_signal = []
@@ -25,7 +46,10 @@ def smooth_signal(signal, window_size):
         smoothed_signal.append(np.nanmean(window))
     return smoothed_signal
 
+def data_to_signal_real_time(accel_type):
+    signal = get_raw_signal(accel_type)
+    return smooth_signal(signal, 6)
+
 def data_to_signal(file_path, accel_type):
     signal = get_signal(file_path, accel_type)
     return smooth_signal(signal, 6)
-
